bdc36f19426cdc7e4af998a7c67dd765b97ffb2c
LA <lyubomir@ch-2.io>
2025-03-20 19:55:16 +0100

    Assessment

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,35 +5,65 @@ import { Venue } from "./api/venue/route";
 import { VenueCard } from "@frontend/components/venueCard";
 import { User } from "./api/user/route";
 import { Footer } from "@frontend/components/footer";
+import { useEffect, useState } from "react";
+
+import { useDebounce } from "@frontend/hooks/debounce.hooks";
+import { useLocalStorage } from "@frontend/hooks/localstorage.hooks";
 
 // If you end up using Promise.all, you will have to implement it yourself
-const promiseAll = function<T>(promises: Promise<T>[]) {
-  // Please implement promise.All don't  just make this a wrapper
-  return new Promise<T[]>((resolve, reject) => {
-    return resolve([]);
-  });
+// const promiseAll = function <T>(promises: Promise<T>[]) {
+//   return new Promise<T[]>((resolve, reject) => {
+//     return resolve([]);
+//   });
+// };
+
+const promiseAll = async <T,>(promises: Promise<T>[]): Promise<T[]> => {
+  return Promise.all(promises);
 };
 
 export default function Home() {
+  const [venues, setVenues] = useState<Venue[]>([]);
+  const [users, setUsers] = useState<User | null>(null);
+  const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(true);
 
-  const venues: Venue[] = [];
-  let user: User | null = null;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const [userResponse, venueResponse] = await promiseAll([
+          fetch("/api/user").then((res) => res.json()),
+          fetch("/api/venue").then((res) => res.json()),
+        ]);
 
+        setUsers(userResponse);
+        setVenues(venueResponse.venues);
+      } catch (err) {
+        console.error("Error fetching data:", err);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-  // make network call to fetch venue
-  // /api/venue
-
-  // After you style the page (Make it responsive)
-
+    fetchData();
+  }, []);
   return (
-    <div>
-      {user && (<NavBar user={user} />)}
-      <main>
-        {venues.map(venue => (
-          <VenueCard key={venue.id} venue={venue} />
-        ))}
+    <div className="min-h-screen flex flex-col bg-gray-50">
+      {users && <NavBar user={users} />}
+      <main className="flex container mx-auto">
+        <input
+          type="text"
+          placeholder="Search ..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          className="w-full p-2 border rounded"
+        />
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          {venues.map((venue) => (
+            <VenueCard key={venue.id} venue={venue} />
+          ))}
+        </div>
       </main>
-        <Footer />
+      <Footer />
     </div>
   );
 }
